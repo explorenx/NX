69e04f0a72f546b03069ba4256aaed4549ce9a90
nxsearch1 <admin@nxsearch.com>
2017-02-07 15:49:48 +0530

    logo link

--- public/app.js
+++ public/app.js
@@ -408,7 +408,7 @@ app.config(function($routeProvider, $locationProvider) {
                 controller : 'dashResultsController',
                
             })
-            .when('/results/:city/:category?', {
+            .when('/showResults/:city/:category?', {
                 templateUrl : 'views/dashboardResults/showResults.html',
                 controller : 'dashResultsController',
                
