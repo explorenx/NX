e56b5187b44c863304b63bb23a5b0606b194cddc
nxsearch1 <admin@nxsearch.com>
2017-03-31 19:10:35 +0530

    OTP added

--- public/controllers/dashResultsController.js
+++ public/controllers/dashResultsController.js
@@ -1244,19 +1244,71 @@ if(area != undefined){
     getData1();
 
  $scope.loading = false;
+ $scope.sendOTP = function (item){
+
+    if(item.usermobile != undefined && item.usermobile.length == 10){
+            $http.defaults.headers.post['Content-Type'] = 'application/json';
+            $http.defaults.headers.post['application-Key'] = 'gJzjVN9NAWY4Tryprooyu3SMTilT8HpfrND8URQ0NDwOO9aDlUlSeR4i7W5o1zjZo0GMhA6np5JNaka4jVYip0aPf6Ke76a5bCuiCIDYOy4LVin-Ju1BCi_mmI7-cPvqn2rv0Mg_qas0UP5M0bNe7w==';
+            $http({
+                method: 'POST',
+                url: 'https://sendotp.msg91.com/api/generateOTP',
+                data :   {
+                            countryCode: "91",
+                            mobileNumber: item.usermobile                    
+                        }
+            }).
+            success(function(status) {
+                //your code when success
+                console.log(status);
+            if(status != indefined && status.status == 'success')
+            {
+                //show popup message that OTP has sent to your mobile number
+            }
+            });
+    }
+ }
+
   $scope.send = function (item){
     $scope.loading = true;
-    $http.post('/sendmail', {
-      from: 'NXsearch <enquiry@nxsearch.com>',
-      to: 'agogweb1@gmail.com,bizzbazar1@gmail.com',
-      subject: 'NXsearch Enquiry for '+ item.ClinicName,
-      //text: item.username + ","+ item.usermobile + ","+item.useremail + ","+item.date + ","+item.time + ","+item.ClinicName,
-      html:"Enquiry for :"+ "<b>"+item.ClinicName+" </b>"+"<br>"+"Name : "+"<b>"+item.username+" </b>"+"<br>"+"Mobile No :"+"<b>"+item.usermobile +"</b>" +"<br>" // html body
-            +"Email Id :"+"<b>"+item.useremail +"</b>" +"<br>" +"Appointment Date :"+"<b>"+item.date +"</b>" +"<br>"+"Appointment Time :"+"<b>"+item.time +"</b>" +"<br>"
-    }).then(res=>{
-        $scope.loading = false;
-        $scope.serverMessage = 'Email sent successfully';
+
+    //validate OTP post Request
+    $http.defaults.headers.post['Content-Type'] = 'application/json';
+    $http.defaults.headers.post['application-Key'] = 'gJzjVN9NAWY4Tryprooyu3SMTilT8HpfrND8URQ0NDwOO9aDlUlSeR4i7W5o1zjZo0GMhA6np5JNaka4jVYip0aPf6Ke76a5bCuiCIDYOy4LVin-Ju1BCi_mmI7-cPvqn2rv0Mg_qas0UP5M0bNe7w==';
+    $http({
+        method: 'POST',
+        url: 'https://sendotp.msg91.com/api/verifyOTP',
+        data :   {
+                    countryCode: "91",
+                    mobileNumber: item.usermobile,
+                    oneTimePassword: item.OTP                    
+                }
+    }).
+    success(function(status) {
+        //your code when success
+        console.log(status);
+       
+       if(status != undefined && status.status == 'success')
+       {
+            $scope.loading = false;
+            $scope.serverMessage = 'Email sent successfully';
+
+            $http.post('/sendmail', {
+                from: 'NXsearch <enquiry@nxsearch.com>',
+                to: 'agogweb1@gmail.com,bizzbazar1@gmail.com',
+                subject: 'NXsearch Enquiry for '+ item.ClinicName,
+                //text: item.username + ","+ item.usermobile + ","+item.useremail + ","+item.date + ","+item.time + ","+item.ClinicName,
+                html:"Enquiry for :"+ "<b>"+item.ClinicName+" </b>"+"<br>"+"Name : "+"<b>"+item.username+" </b>"+"<br>"+"Mobile No :"+"<b>"+item.usermobile +"</b>" +"<br>" // html body
+                        +"Email Id :"+"<b>"+item.useremail +"</b>" +"<br>" +"Appointment Date :"+"<b>"+item.date +"</b>" +"<br>"+"Appointment Time :"+"<b>"+item.time +"</b>" +"<br>"
+                }).then(res=>{
+                    $scope.loading = false;
+                    $scope.serverMessage = 'Email sent successfully';
+            });
+       }
+    }).
+    error(function(status) {
+        //your code when fails
     });
+
   }
 
     // delete a contact after checking it
